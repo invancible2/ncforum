a105e346e1f74e52b9d3210c85800f6ef43eeeab
Ivan Ramos <ivanramos.se@gmail.com>
2023-03-24 11:31:23 +0800

    add sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/*.html"
+    "./templates/*.html",
+    "./core/templates/core/*.html"
   ],
   theme: {
     fontFamily: {
@@ -10,7 +11,26 @@ module.exports = {
       openSansBold: 'OpenSansBold',
       sourceSansPro: 'SourceSansPro',
     },
-    extend: {},
+    extend: {
+      width: {
+        '10p': '10%',
+        '18p': '18%',
+        '20p': '20%',
+        '30p': '30%',
+        '40p': '40%',
+        '50p': '50%',
+        '60p': '60%',
+        '70p': '70%',
+        '80p': '80%',
+        '90p': '90%',
+      },
+      colors: {
+        primary: '#125615',
+        secondary: '#BEBDBD',
+        tertiary: '#FFC805',
+        background: '#EBEBEB',
+      },
+    },
   },
   plugins: [],
 }
