b59722595c93a2a7264b8e5ce4ddae90d5657c97
Ivan Ramos <ivanramos.se@gmail.com>
2023-03-24 10:59:25 +0800

    add fonts & css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./templates/*.html"
   ],
   theme: {
+    fontFamily: {
+      openSans: 'OpenSans',
+      openSansMedium: 'OpenSansMedium',
+      openSansBold: 'OpenSansBold',
+      sourceSansPro: 'SourceSansPro',
+    },
     extend: {},
   },
   plugins: [],
